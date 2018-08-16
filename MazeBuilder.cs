969f63168dd1f6c10fc97485c07c31af8f3f7056
wmadwand <wmadwand@gmail.com>
2018-08-16 16:21:18 +0300

    Several classes

---
+++ MazeBuilder.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Builder
+{
+    #region MazeParts
+    public enum Direction
+    {
+        North, South, East, West
+    }
+
+    public class MapSite { }
+
+    public class Maze
+    {
+        private List<Room> _rooms;
+
+        public Maze()
+        {
+            _rooms = new List<Room>();
+        }
+
+        public void AddRoom(Room room)
+        {
+            _rooms.Add(room);
+        }
+
+        public Room GetRoom(int number)
+        {
+            return _rooms[number];
+        }
+    }
+
+    public class Room : MapSite
+    {
+        private int _number;
+        private MapSite[] _sides = new MapSite[4];
+
+        public Room(int number)
+        {
+            _number = number;
+        }
+
+        public void SetSide() { }
+    }
+
+    public class Wall : MapSite
+    {
+
+    }
+
+    public class Door : MapSite { }
+    #endregion
+
+    public class MazeBuilder
+    {
+        public virtual void BuildMaze() { }
+        public virtual void BuildRoom(int room) { }
+        public virtual void BuildDoor(int roomFrom, int roomTo) { }
+
+        public virtual Maze GetMaze() { return null; }
+
+        protected MazeBuilder()
+        {
+        }
+    }
+
+    public class StandartMazeBuilder : MazeBuilder
+    {
+        private Maze _currentMaze;
+        //private Direction CommonWall(Room r1, Room r2);
+
+        public override void BuildMaze()
+        {
+            _currentMaze = new Maze();
+        }
+
+        public override Maze GetMaze()
+        {
+            return _currentMaze;
+        }
+    }
+}
---
+++ MazeGame.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Builder
+{
+    public class MazeGame
+    {
+        public MazeGame()
+        {
+        }
+
+        public Maze CreateMaze(MazeBuilder builder)
+        {
+            builder.BuildMaze();
+
+            builder.BuildRoom(1);
+            builder.BuildRoom(2);
+            builder.BuildDoor(1, 2);
+
+            return builder.GetMaze();
+        }
+
+        public Maze CreateComplexMaze(MazeBuilder builder)
+        {
+            builder.BuildMaze();
+
+            builder.BuildRoom(1);
+            builder.BuildRoom(100);
+
+            return builder.GetMaze();
+        }
+    }
+}
